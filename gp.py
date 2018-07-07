76f28fc9b5b291f2cb21656716c4785accb7e4c0
Fabian Otto <fabi.otto@t-online.de>
2018-07-07 21:42:00 +0200

    gp

---
+++ gp.py
@@ -0,0 +1,89 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+plt.rcParams['figure.figsize'] = (10.0, 8.0)
+
+
+def target(x):
+    s = np.sin(x)
+    return s + np.square(s)
+
+
+def kernel(x, z):
+    # if type(x) in (np.float32, float):
+    # return theta_1 * np.exp(-.5 * theta_2 * np.abs(x - z) ** 2 / 2)
+
+    return np.exp(-(x - z) ** 2)
+
+
+def compute_c(x, noice):
+    return kernel(x, x) + noice
+
+
+def predict(x, X, Y, C_inv, noice):
+    k = np.array([kernel(x, val) for val in X])
+    mean = k.T.dot(C_inv).dot(Y)
+    std = np.sqrt(compute_c(x, noice) - k.T.dot(C_inv).dot(k))
+    return mean, std
+
+
+def plot_gp(X, mean, std, iteration):
+    y1 = mean - 2 * std
+    y2 = mean + 2 * std
+
+    plt.plot(X, target(X), "-", color="red", label="$sin(x)\cdot sin^2(x)$")
+    plt.plot(X, mean, color="black", label="$\mu$")
+    plt.fill_between(X, y1, y2, facecolor='lightblue', interpolate=True, alpha=.5, label="$2\sigma$")
+    plt.title("$\mu$ and $\sigma$ for iteration={}".format(iteration))
+    plt.xlabel("x")
+    plt.ylabel("y")
+
+
+def gpr():
+    noice = 0.001
+    step_size = 0.005
+    X = np.arange(0, 2 * np.pi + step_size, step_size)
+    iterations = 16
+
+    std = np.array([np.sqrt(compute_c(x, noice)) for x in X])
+    j = np.argmax(std)
+
+    Xn = np.array([X[j]])
+    Yn = np.array([target(Xn)])
+
+    C = np.array(compute_c(X[j], noice)).reshape((1, 1))
+    C_inv = np.linalg.inv(C)
+
+    for iteration in range(0, iterations):
+        mean = np.zeros(X.shape[0])
+        std = np.zeros(X.shape[0])
+
+        for i, x in enumerate(X):
+            mean[i], std[i] = predict(x, Xn, Yn, C_inv, noice)
+
+        if iteration + 1 in [1, 2, 4, 8, 16]:
+            plot_gp(X, mean, std, iteration + 1)
+            plt.plot(Xn, Yn, "o", c="blue", label="sampled")
+            plt.legend()
+            plt.show()
+
+        j = np.argmax(std)
+        Xn = np.append(Xn, X[j])
+        Yn = np.append(Yn, target(Xn[-1]))
+
+        # update C matrix
+        x_new = Xn[-1]
+        k = np.array([kernel(x_new, val) for val in Xn])
+        c = kernel(x_new, x_new) + noice
+
+        dim = C.shape[0]
+        C_new = np.empty((dim + 1, dim + 1))
+        C_new[:-1, :-1] = C
+        C_new[-1, -1] = c
+        C_new[:, -1] = k
+        C_new[-1:] = k.T
+
+        C = C_new
+        C_inv = np.linalg.inv(C)
+
+gpr()
--- svm.py
+++ svm.py
@@ -1,8 +1,11 @@
+import re
+
 import numpy as np
 from cvxopt import matrix, solvers
-import re
 from matplotlib import pyplot as plt
+
 plt.rcParams['figure.figsize'] = (10.0, 8.0)
+
 
 def read_iris_data(filepath):
     with open(filepath, 'r', encoding='utf8') as f:
@@ -12,15 +15,17 @@ def read_iris_data(filepath):
             splitline = re.split('[ ]+', line.strip())
             x.append(np.asarray([float(f) for f in splitline[:-1]]))
             y.append(1.0 if splitline[2] == '0' else -1.0)
-            
+
         return np.asarray(x), np.asarray(y)
-    
-    
-def rbf_kernel(x, z, sigma=0.80):
-    return np.exp(-(np.linalg.norm(x - z, ord=2) ** 2) / (2 * sigma**2))
+
+
+def rbf_kernel(x, z, sigma=.4):
+    """gaussian kernel"""
+    return np.exp(-(np.linalg.norm(x - z, ord=2) ** 2) / (2 * sigma ** 2))
+
 
 def plot_data(x, y):
-    #TODO: extend to show margin and misclassifications
+    # TODO: extend to show margin and misclassifications
     plt.figure()
     c = []
     for label in y:
@@ -28,38 +33,58 @@ def plot_data(x, y):
             c.append('blue')
         else:
             c.append('orange')
-            
-    plt.scatter(x[:,0], x[:,1], c=c)
+
+    plt.scatter(x[:, 0], x[:, 1], c=c)
     plt.xlabel("x1")
     plt.ylabel("x2")
     plt.title("Iris dataset")
-    #plt.legend()
-    
-    
+    # plt.legend()
+
+
 class SVM:
     def __init__(self, kernel, C, min_sv_alpha=0.00001):
         self.kernel = kernel
         self.C = C
+        # because of numpy alphas never get 0
         self.min_sv_alpha = min_sv_alpha
-        
+
     def fit(self, X, y):
         if type(X) == 'list':
             X = np.asarray(X)
-            
+
         if type(y) == 'list':
             y = np.asarray(y)
-            
+
         # evaluate kernel function for all pairs and buffer results
         # (similar to kernel PCA as described by Bishop)
         # TODO: might this one get prohibitively large? Need scipy sparse matrix?
         self.K = np.ndarray(shape=(X.shape[0], X.shape[0]))
         for i, xi in enumerate(X):
             for j, xj in enumerate(X):
-                self.K[i][j] = self.kernel(xi, xj)
-                
+                self.K[i, j] = self.kernel(xi, xj)
+
         # solve lagrangian dual using cvxopt quadratic optimization
         # note that the "matrix" objects are cvxopt objects, not numpy
         # TODO: bias!
+
+        # general SVM
+        # Q = matrix(np.outer(y, y) * self.K)
+        # p = matrix(np.ones(self.K.shape[0]) * -1)
+        # A = matrix(y, (1, self.K.shape[0]))
+        # b = matrix(0.0)
+        #
+        # tmp1 = np.diag(np.ones(self.K.shape[0]) * -1)
+        # tmp2 = np.identity(self.K.shape[0])
+        # G_x = matrix(np.vstack((tmp1, tmp2)))
+        # tmp1 = np.zeros(self.K.shape[0])
+        # tmp2 = np.ones(self.K.shape[0]) * self.C
+        # h_x = matrix(np.hstack((tmp1, tmp2)))
+        #
+        # G_slack = matrix(np.diag(np.ones(self.K.shape[0])))
+        # h_slack = matrix(np.ones(self.K.shape[0]) * self.C)
+        # G = matrix(np.vstack((G_x, G_slack)))
+        # h = matrix(np.vstack((h_x, h_slack)))
+
         Q = matrix(np.outer(y, y) * self.K)
         p = matrix(np.ones(self.K.shape[0]) * -1)
         G_x = matrix(np.diag(np.ones(self.K.shape[0]) * -1))
@@ -68,36 +93,51 @@ class SVM:
         h_slack = matrix(np.ones(self.K.shape[0]) * self.C)
         G = matrix(np.vstack((G_x, G_slack)))
         h = matrix(np.vstack((h_x, h_slack)))
-        
+
         A = matrix(y, (1, self.K.shape[0]))
         b = matrix(0.0)
-        
+
         solution = solvers.qp(Q, p, G, h, A, b)
 
         # the solver outputs the alphas (lagrange multipliers)
-        self.alphas = np.asarray(solution['x'])[:,0]
+        self.alphas = np.asarray(solution['x'])[:, 0]
         print('Lagrange multipliers:')
         print(self.alphas)
-        
+
         # save the support vectors (i.e. the ones with non-zero alphas) and their y values
-        sv_index = np.where(self.alphas > self.min_sv_alpha)
+        sv_index = np.where(self.min_sv_alpha < self.alphas)
         print('\nNumber of support vectors:', sv_index[0].shape[0], end='\n\n')
+        self.support_alphas = self.alphas[sv_index]
         self.support_vectors = X[sv_index]
         self.targets = y[sv_index]
-        
-        
+
+        # estimate the bias (Bishop page 334)
+
+        self.b = 0
+        for i, yi in enumerate(self.targets):
+            m = yi
+            for j, xj in enumerate(self.support_vectors):
+                m -= self.alphas[j] * self.targets[j] * self.kernel(self.support_vectors[i], xj)
+            self.b += m
+        self.b /= self.targets.shape[0]
+
+        print('bias:', self.b)
+
     def predict(self, X):
         scores = np.ndarray(shape=(X.shape[0]))
         for i, xi in enumerate(X):
             for j in range(self.support_vectors.shape[0]):
-                scores[i] += self.alphas[j] * self.targets[j] * self.kernel(xi, self.support_vectors[j])
-                
+                scores[i] += self.support_alphas[j] * self.targets[j] * self.kernel(xi, self.support_vectors[j])
+
+            scores[i] += self.b
+
         return scores
-    
+
+
 # run experiment on the reduced iris dataset
 X, y = read_iris_data('iris-pca.txt')
 plot_data(X, y)
-svm = SVM(rbf_kernel, 42)
+svm = SVM(rbf_kernel, C=5)
 svm.fit(X, y)
 y_hat = svm.predict(X)
 pred = np.ndarray(shape=y_hat.shape)
@@ -112,3 +152,43 @@ tp = (y == pred).sum()
 fn = X.shape[0] - tp
 print('Number of misclassified data points:', fn)
 print('Accuracy:', tp / X.shape[0])
+
+# plot the result!
+plt.figure()
+c = []
+for i, label in enumerate(y):
+    if label == 1.0:
+        if pred[i] == label:
+            c.append('blue')
+        else:
+            c.append('fuchsia')
+    else:
+        if pred[i] == label:
+            c.append('orange')
+        else:
+            c.append('red')
+
+plt.scatter(X[:, 0], X[:, 1], c=c)
+# plt.scatter(svm.support_vectors[:, 0], svm.support_vectors[:, 1], c="red", marker="x")
+# plt.scatter(X[:, 0], X[:, 1], "x", c=c)
+# plt.scatter(X[:, 0], X[:, 1], ".", c=c
+
+plt.xlabel("x1")
+plt.ylabel("x2")
+plt.title("Iris dataset with decision boundary")
+# plt.show()
+
+steps = 200
+x_range = np.linspace(-1.5, 3, steps)
+y_range = np.linspace(-3, 3, steps)
+x_values, y_values = np.meshgrid(x_range, y_range)
+Z = np.zeros((steps, steps))
+
+# Plot the decision boundary. For that, we will assign a color to each
+# point in the mesh [x_min, m_max]x[y_min, y_max].
+Z = svm.predict(np.c_[x_values.ravel(), y_values.ravel()])
+
+# Put the result into a color plot
+Z = Z.reshape(x_values.shape)
+plt.contour(x_values, y_values, Z, cmap=plt.cm.Paired)
+plt.show()
